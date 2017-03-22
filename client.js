c5ae22165450c8d679dd5bb4e11149c4770ca7d5
bhargav <bhargavbhegde7@gmail.com>
2017-03-22 22:23:36 +0530

    made two different files for light

--- client.js
+++ client.js
@@ -1,32 +1,21 @@
-var gpio = require("gpio");
-//var socket = require('socket.io-client')('http://fbchatbot-96204.app.xervo.io');
-var socket = require('socket.io-client')('https://fbchatbot1.appspot.com');
-
-var turnLightOn = function(pinNumber){
-    gpioPin = gpio.export(pinNumber,{
-            ready: function(){
-              gpioPin.set();
-              setTimeout(function() {
-                gpioPin.reset();
-              }, 1000 );
-            }
-    });
-  }
+//var socket = require('socket.io-client')('https://fbchatbot1.appspot.com');
+var socket = require('socket.io-client')('http://localhost:3000');
 
 socket.on('connect', function(){
 	console.log("connect emitted from server");
 });
+
 socket.on('fb-message', function(data){
-	var msg = JSON.parse(data).text;
+
+  var msg = "";
+  try{
+    msg = JSON.parse(data).text;
+  }catch(err){
+    msg = data.text;
+  }
+
 	console.log(msg);
 
-	if(msg.indexOf('green') > -1){
-		turnLightOn(18);
-	}
-
-	if(msg.indexOf('red') > -1){
-		turnLightOn(16);
-	}
 });
 socket.on('disconnect', function(){
 	console.log("disconnected!");
--- trylight.js
+++ trylight.js
@@ -1,4 +1,7 @@
 var gpio = require("gpio");
+var socket = require('socket.io-client')('https://fbchatbot1.appspot.com');
+//var socket = require('socket.io-client')('http://localhost:3000');
+
 
 var turnLightOn = function(pinNumber){
     gpioPin = gpio.export(pinNumber,{
@@ -6,9 +9,34 @@ var turnLightOn = function(pinNumber){
               gpioPin.set();
               setTimeout(function() {
                 gpioPin.reset();
-              }, 30000 );
+              }, 1000 );
             }
     });
   }
 
-turnLightOn(16);
+socket.on('connect', function(){
+	console.log("connect emitted from server");
+});
+
+socket.on('fb-message', function(data){
+
+  var msg = "";
+  try{
+    msg = JSON.parse(data).text;
+  }catch(err){
+    msg = data.text;
+  }
+
+	console.log(msg);
+
+	if(msg.indexOf('green') > -1){
+		turnLightOn(18);
+	}
+
+	if(msg.indexOf('red') > -1){
+		turnLightOn(16);
+	}
+});
+socket.on('disconnect', function(){
+	console.log("disconnected!");
+});
